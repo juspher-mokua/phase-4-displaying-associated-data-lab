6ad254c8d0c7b0c6c7c6c37fdb8daa055493441c
Juspher Mokua <juspermokua6@gmail.com>
2022-12-12 01:36:54 +0300

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,14 @@
 class ItemsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :handle_error
+    def index 
+        item  = Item.all
+        render json: item, include: :user, status: :ok
+    end
+
+
+
+    private 
+    def handle_error 
+        render json: {error: "Item not found"}
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :handle_error
+    def show 
+        user  = User.find_by(id: params[:id])
+        render json: user, include: :items, status: :ok
+    end
+
+
+
+    private 
+    def handle_error 
+        render json: {error: "User not found"}
+    end
+
 end
